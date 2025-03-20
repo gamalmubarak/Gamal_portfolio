eef5ddfaa56d7ed21efad6a93e22918350b6d730
gamalmubarak <eng-gemy@hotmail.com>
2025-03-20 11:10:36 -0400

    Header component added

---
+++ React Portfolio/src/header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Navigation from './navigation';
+
+const Header = () => {
+  return (
+    <header className="mb-4">
+      <h1>Gamal Mubarak</h1>
+      <Navigation />
+    </header>
+  );
+};
+
+export default Header;
