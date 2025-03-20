254699e2c2fd227c7b5ecec7e8ee2fc13ee7848d
gamalmubarak <eng-gemy@hotmail.com>
2025-03-20 11:19:04 -0400

    Footer component added to the components folder

---
+++ React Portfolio/src/components/footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="mt-4">
+      <ul className="list-inline text-center">
+        <li className="list-inline-item">
+          <a href="https://github.com/gamalmubarak" target="_blank" rel="noopener noreferrer">GitHub</a>
+        </li>
+        <li className="list-inline-item">
+          <a href="https://www.linkedin.com/in/gamalmubarak87" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+        </li>
+        <li className="list-inline-item">
+          <a href="https://twitter.com/yourusername" target="_blank" rel="noopener noreferrer">Twitter</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
