b816f638647f543979e4897e6eacf92c582c5a56
gamalmubarak <eng-gemy@hotmail.com>
2025-03-20 12:08:06 -0400

    code updated

--- React Portfolio/src/App.jsx
+++ React Portfolio/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/header';
 import Footer from './components/footer';
 import AboutMe from './pages/AboutMe';
