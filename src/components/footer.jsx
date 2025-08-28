fdf4b8b4a9943a069f9e9db91f8a42a5c58a28c8
gamalmubarak <eng-gemy@hotmail.com>
2025-08-28 14:22:47 -0400

    updated footer.jsx added icons for social link

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -5,13 +5,19 @@ const Footer = () => {
     <footer className="mt-4">
       <ul className="list-inline text-center">
         <li className="list-inline-item">
-          <a href="https://github.com/gamalmubarak" target="_blank" rel="noopener noreferrer">GitHub</a>
+          <a href="https://github.com/gamalmubarak" target="_blank" rel="noopener noreferrer" className="text-decoration-none">
+            <i className="bi bi-github me-2"></i>GitHub
+          </a>
         </li>
         <li className="list-inline-item">
-          <a href="https://www.linkedin.com/in/gamalmubarak87" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+          <a href="https://www.linkedin.com/in/gamalmubarak87" target="_blank" rel="noopener noreferrer" className="text-decoration-none">
+            <i className="bi bi-linkedin me-2"></i>LinkedIn
+          </a>
         </li>
         <li className="list-inline-item">
-          <a href="mailto:gamalmubarak87@gmail.com" target="_blank" rel="noopener noreferrer">Email</a>
+          <a href="mailto:gamalmubarak87@gmail.com" target="_blank" rel="noopener noreferrer" className="text-decoration-none">
+            <i className="bi bi-envelope me-2"></i>Email
+          </a>
         </li>
       </ul>
     </footer>
